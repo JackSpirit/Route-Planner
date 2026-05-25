5cd67fc414093620018df8031198b58755480265
JackSpirit <jacksonspirit755@gmail.com>
2026-05-25 23:40:56 +0530

    feat: implement parallel PBF map streaming and element counting

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module Route_Planner
 
 go 1.26
+
+require (
+	github.com/qedus/osmpbf v1.2.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,19 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"runtime"
+    "github.com/qedus/osmpbf"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	file, err := os.Open("delaware-260524.osm.pbf")
+	if err != nil {
+		fmt.Printf("Error opening PBF file: %v\n", err)
+		return
 	}
+	defer file.Close()
+
+	decoder := osmpbf.NewDecoder(file)
+
+	decoder.SetBufferSize(osmpbf.MaxBlobSize)
+	err = decoder.Start(runtime.NumCPU())
+	if err != nil {
+		fmt.Printf("Error starting decoder: %v\n", err)
+		return
+	}
+
+	var nodeCount int64
+	var wayCount int64
+
+	fmt.Println("Streaming binary blocks")
+
+	for {
+		v, err := decoder.Decode(); 
+		if err == io.EOF {
+			break
+		} else if err!= nil {
+			fmt.Printf("Error during streaming: %v\n", err)
+			return
+		} else {
+			switch v.(type) {
+			case *osmpbf.Node:
+				nodeCount++
+			case *osmpbf.Way:
+			    wayCount++
+			}
+		}
+	}
+
+	fmt.Printf("\n Success! Binary Scanning Complete")
+	fmt.Printf("Total Nodes Processed: %d\n", nodeCount)
+	fmt.Printf("Total Ways Processed: %d\n", wayCount)
 }
